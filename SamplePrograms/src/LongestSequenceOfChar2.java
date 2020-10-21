1da2433e28e77e8578b42220380474d33027675e
unknown <nikhil.udgirkar@gmail.com>
2020-10-21 21:08:44 +0530

    added new programs

---
+++ SamplePrograms/src/LongestSequenceOfChar2.java
@@ -0,0 +1,15 @@
+
+public class LongestSequenceOfChar2 {
+	public static void main(String[] args) {
+		String s="hello world";
+		
+		int y=0;
+		String result=" ";
+		for (int x=0; x<s.length(); x++) {
+			if (s.charAt(x)==s.charAt(y))
+				result=result+s.charAt(x);
+			y++;
+		}
+		System.out.println(result);
+	}
+}
